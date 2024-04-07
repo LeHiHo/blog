31ac572682ef64cfd253182ee9c019d36995302c
Hojin <wkdghwls0623@gmail.com>
2024-04-08 00:00:00 +0900

    Feat: Prism 플러그인 추가

--- src/components/Editor/MarkdownEditor.tsx
+++ src/components/Editor/MarkdownEditor.tsx
@@ -2,9 +2,23 @@
 
 import React, { useState, useEffect, ChangeEvent } from 'react';
 import { marked } from 'marked';
-
 import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css'; // _document.tsx 파일에 추가하거나 여기에 임포트할 수 있습니다.
+// 언어
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-python';
+// 테마
+import 'prismjs/themes/prism-tomorrow.css';
+
+import 'prismjs/plugins/autolinker/prism-autolinker';
+import 'prismjs/plugins/command-line/prism-command-line';
+// import 'prismjs/plugins/download-button/prism-download-button';
+import 'prismjs/plugins/highlight-keywords/prism-highlight-keywords';
+import 'prismjs/plugins/show-language/prism-show-language';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+import 'prismjs/plugins/inline-color/prism-inline-color';
+import 'prismjs/plugins/previewers/prism-previewers';
+import 'prismjs/plugins/toolbar/prism-toolbar';
 
 const MarkdownEditor: React.FC = () => {
   const [text, setText] = useState<string>('');
@@ -13,9 +27,10 @@ const MarkdownEditor: React.FC = () => {
     setText(e.target.value);
   };
 
+  // getMarkdownText 함수 내에서 marked 사용
   const getMarkdownText = (): { __html: string } => {
     const rawMarkup = marked(text, {
-      highlight: function (code, lang) {
+      highlight: function (code: string, lang: string): string {
         // TypeScript를 사용하면서 Prism.languages[lang]의 존재 여부를 체크
         const language = Prism.languages[lang] ? lang : 'markup';
         return Prism.highlight(code, Prism.languages[language], language);
