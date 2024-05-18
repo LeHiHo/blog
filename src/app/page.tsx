2d56926932d77f90c01ab00fa57ea848dae155ee
Hojin <wkdghwls0623@gmail.com>
2024-05-18 23:55:17 +0900

    feat: 프로필 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,21 @@ export default function Home() {
   return (
     <>
       <header>
-        <div className="flex justify-between m-1">
+        <div className="flex justify-between m-1 items-center">
           <h1>lehiho's blog</h1>
           <ModeToggle />
         </div>
       </header>
-      <main className="flex items-center justify-center">안녕하세요</main>
+      <main className="flex items-center justify-center">
+        <div className="flex">
+          <div className=" bg-red-800 p-10 rounded-full"></div>
+          <div className="ml-5">
+            <p>장호진</p>
+            <p>기억보다 기록을</p>
+            <p>각종 url</p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
