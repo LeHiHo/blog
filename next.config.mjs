1ed7b5a88693fcf3d5c6768a82d085ea89bda449
Hojin <wkdghwls0623@gmail.com>
2024-05-30 00:59:12 +0900

    Fix: github.io 배포 롤백

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  exportTrailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
